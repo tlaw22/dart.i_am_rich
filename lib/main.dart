a5817aaefb32400cae9ce0c3e477061840816352
Tim <timothy.lawless@gmail.com>
2023-05-09 15:01:52 -0400

    adding files

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,17 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(backgroundColor: Colors.limeAccent[100],
+
         appBar: AppBar(
-          title: Text('Nonsense - I am Rich'),
+          title: Text('Nonsense - I am Poor'),
             backgroundColor: Colors.blueGrey[900],
+
         ),
         body: const Center(
           child: Image(
             image: AssetImage('images/diamond.png'),
+
+
           ),
         ),
       ),
